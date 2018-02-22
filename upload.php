09d0f5cbdd107e683059cd467331259af23dc458
NabilaSheikh <nabila2512@gmail.com>
2018-02-22 10:02:14 +0100

    uu

--- upload.php
+++ upload.php
@@ -1,9 +1,5 @@
 <?php
 
-  header('Access-Control-Allow-Origin: *');
-
-//echo 'ok' ; exit;
-
     /*var_dump ($_FILES['file']['name']);*/
      
 //echo $_FILES['file']['name'];
@@ -19,9 +15,7 @@
     }
     else {
         move_uploaded_file($_FILES['file']['tmp_name'], 'uploads/' . $final_name);
-        echo json_encode($final_name);
+        echo $final_name;
     }
-
-   // $json = json_encode($final_name);
 
 ?>
